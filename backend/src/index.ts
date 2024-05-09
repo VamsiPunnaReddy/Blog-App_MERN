caece9c4014c14097ad2ae3b91e07031be4ea834
VamsiPunnaReddy <vaaamsi1234@gmail.com>
2024-05-09 11:46:00 +0530

    added
    try catch for not breaking the backend when somthing happens in a route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter)
-app.use("/api/v1/blog", blogRouter)
+app.use("/api/v1/blogs", blogRouter)
 
 app.get("/", (req, res) => {
     res.status(200).json({ hello: "World!" })
